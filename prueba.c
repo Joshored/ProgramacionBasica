209c86e1c4854d4d821b2027752dc6d0c63836e2
Produ <jorgelpr.lh@gmail.com>
2026-05-30 12:04:27 -0600

    subida de ejercicios basicos para c y python

---
+++ prueba.c
@@ -0,0 +1,55 @@
+#include <stdio.h> // biblioteca de entrada y salida estandar de c
+// Hola mundo
+
+#include <stdio.h>
+
+//funcion para sumar
+//int sumar(int a, int b) {
+//    return a + b;
+//}
+
+int main() {
+	// hola mundo
+    printf("Hola mundo\n");
+
+	//input, output
+   // char nombre[50];
+
+   // printf("¿Cómo te llamas? ");
+   // scanf("%49s", nombre);
+
+    //printf("Hola %s\n", nombre);
+
+	//if
+    //int edad = 17;
+
+    //if (edad >= 18) {
+       // printf("Mayor de edad\n");
+    //} else {
+      //  printf("Menor de edad\n");
+    //}
+
+	//while
+    //int contador = 1;
+
+    //while (contador <= 5) {
+      //  printf("%d\n", contador);
+       // contador++;
+    //}
+
+	//for
+    //for (int i = 0; i < 10; i++) {
+      //  printf("%d\n", i);
+    //}
+
+	//areglos
+    //int numeros[3] = {10, 20, 30};
+	//recorrido de arreglos
+    //for (int i = 0; i < 3; i++) {
+      //  printf("%d\n", numeros[i]);
+    //}
+	// funcion de suma
+    //printf("5 + 3 = %d\n", sumar(5, 3));
+
+    return 0;
+}
---
+++ prueba.py
@@ -0,0 +1,28 @@
+print("hola mundo") #print palabra reservada para imprimir
+#input / output
+#nombre = input("¿Cómo te llamas? ")
+#print("Hola", nombre)
+#if
+#edad = 18
+
+#if edad >= 18:
+ #   print("Mayor de edad")
+#else:
+ #   print("Menor de edad")
+#while
+#contador = 1
+
+#while contador <= 5:
+ #   print(contador)
+  #  contador += 1
+#for
+#for i in range(10):
+ #   print(i)
+#funciones
+#def sumar(a, b):
+ #   return a + b
+#arreglos
+#numeros = [10, 20, 30]
+
+#for numero in numeros:
+ #   print(numero)
